597225b999d1921aa64dae97520a89b9a298ef95
Claudio Desideri <happy.snizzo@gmail.com>
2020-12-24 17:27:17 +0100

    Added support for while(){} construct

--- parserlexer.py
+++ parserlexer.py
@@ -11,7 +11,9 @@ reserved = {
 }
 
 # List of token names.   This is always required
-tokens = (
+tokens = [
+'LCURLY',
+'RCURLY',
 'NUMBER',
 'PLUS',
 'MINUS',
@@ -22,7 +24,6 @@ tokens = (
 'DIVIDE',
 'LPAREN',
 'RPAREN',
-'NAME',
 'newline',
 # 'QUOTE',
 'STRING',
@@ -30,8 +31,9 @@ tokens = (
 'MINUSEQUALS',
 'TIMESEQUALS',
 'DIVIDEEQUALS',
-'COMMA'
-)
+'COMMA',
+'NAME'
+] + list(reserved.values())
 
 # Regular expression rules for simple tokens
 t_PLUS    = r'\+'
@@ -49,11 +51,15 @@ t_MINUSEQUALS = r'-='
 t_TIMESEQUALS = r'\*='
 t_DIVIDEEQUALS = r'/='
 t_COMMA   = r','
+t_LCURLY = r'\{'
+t_RCURLY = r'\}'
 
 # matching reserved words
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.value = str(t.value)
+    if str(t.value).lower() in reserved.keys():
+        t.type = reserved[str(t.value).lower()]
     return t
 
 # A regular expression rule with some action code
@@ -102,40 +108,69 @@ names = { }
 
 
 def p_statements(t):
-    '''statements : statement newline statements
-                    | statement newline
+    '''statements :   newline statements
+                    | statement statements
                     | statement'''
+    vmi.debug(t,"MAIN")
+
+def p_statement_nl(t):
+    '''statement : statement newline'''
+    vmi.debug(t,"NL")
 
 def p_statement_assign(t):
     '''statement : NAME EQUALS expression'''
+    vmi.debug(t,"ASSIGN")
     vmi.stack.addVariable(t[1],t[3])
     vmi.encode("set {} {}".format(t[1],t[3]))
+    # releasing temp var
+    vmi.stack.releaseTempVar(t[3])
 
 def p_statement_expr(t):
     'statement : expression'
-    print(t[1])
+    vmi.debug(t,"EXPR")
+    #print(t[1])
+
+def p_statement_while(t):
+    '''statement : beforewhile WHILE expression LCURLY statements RCURLY'''
+    vmi.debug(t,"REDUCEWHILE")
+    jumpto = vmi.closeBlock()
+    vmi.encode("jump {} {}".format(jumpto,t[3]))
+
+def p_before_while(t):
+    '''beforewhile :'''
+    vmi.debug(t, "SHIFTWHILE")
+    vmi.openBlock()
 
 def p_statement_relop_greater_than(t):
-    '''statement : expression GREATER expression'''
-    vmi.encode("{} {} {}".format(t[1],t[2],t[3]))
+    '''expression : expression GREATER expression'''
+    vmi.debug(t,"GREATERTHAN")
+    expr = "greaterThan {} {}".format(t[1],t[3])
+    t[0] = expr
 
 def p_statement_relop_less_than(t):
-    '''statement : expression LESS expression'''
-    vmi.encode("{} {} {}".format(t[1],t[2],t[3]))
+    '''expression : expression LESS expression'''
+    vmi.debug(t,"LESSTHAN")
+    expr = "lessThan {} {}".format(t[1],t[3])
+    t[0] = expr
 
 def p_statement_relop_greater_equal(t):
-    '''statement : expression GREATER EQUALS expression'''
-    vmi.encode("{} {}{} {}".format(t[1],t[2],t[3],t[4]))
+    '''expression : expression GREATER EQUALS expression'''
+    vmi.debug(t,"GREATEREQUAL")
+    expr = "greaterThanEq {} {}".format(t[1],t[4])
+    t[0] = expr
 
 def p_statement_relop_less_equal(t):
-    '''statement : expression LESS EQUALS expression'''
-    vmi.encode("{} {}{} {}".format(t[1],t[2],t[3],t[4]))
+    '''expression : expression LESS EQUALS expression'''
+    vmi.debug(t,"LESSEQUAL")
+    expr = "lessThanEq {} {}".format(t[1],t[4])
+    t[0] = expr
 
 def p_binop_generic(t):
     '''expression : expression PLUS expression
                 | expression MINUS expression
                 | expression TIMES expression
                 | expression DIVIDE expression'''
+    vmi.debug(t,"BINOP_GENERIC")
     
     vname = vmi.stack.requestTempVar()
     if t[2] == '+'  : vmi.encode("op add {} {} {}".format(vname, t[1],t[3]))
@@ -148,34 +183,41 @@ def p_binop_generic(t):
 
 def p_statement_funcall(t):
     '''statement : NAME LPAREN expressions RPAREN'''
+    vmi.debug(t,"FUNCALL")
     vmi.funcall(t[1],t[3])
 
 
 def p_statement_binop_shortadd(t):
     '''statement :    NAME PLUSEQUALS expression'''
+    vmi.debug(t,"BINOP_PLUSEQUALS")
     vmi.encode("op add {} {} {}".format(t[1],t[1],t[3]))
 
 
 def p_statement_binop_shortsub(t):
     '''statement :    NAME MINUSEQUALS expression'''
+    vmi.debug(t,"BINOP_MINUSEQUALS")
     vmi.encode("op sub {} {} {}".format(t[1],t[1],t[3]))
 
 
 def p_statement_binop_shortmul(t):
     '''statement :    NAME TIMESEQUALS expression'''
+    vmi.debug(t,"BINOP_TIMESEQUALS")
     vmi.encode("op mul {} {} {}".format(t[1],t[1],t[3]))
 
 
 def p_statement_binop_shortdiv(t):
     '''statement :    NAME DIVIDEEQUALS expression'''
+    vmi.debug(t,"BINOP_SHORTDIV")
     vmi.encode("op div {} {} {}".format(t[1],t[1],t[3]))
 
 def p_expressions_list(t):
     '''expressions : expression COMMA expressions'''
+    vmi.debug(t,"LIST")
     t[0] = t[1]+" "+t[3]
 
 def p_expressions_void(t):
     '''expressions : expression'''
+    vmi.debug(t,"VOID")
     t[0] = t[1]
 
 def p_expression_uminus(t):
@@ -184,18 +226,22 @@ def p_expression_uminus(t):
 
 def p_expression_group(t):
     'expression : LPAREN expression RPAREN'
+    vmi.debug(t,"GROUP")
     t[0] = t[2]
 
 def p_expression_number(t):
     'expression : NUMBER'
+    vmi.debug(t,"NUMBER")
     t[0] = t[1]
 
 def p_expression_string(t):
     'expression : STRING'
+    vmi.debug(t,"STRING")
     t[0] = t[1]
 
 def p_expression_name(t):
     'expression : NAME'
+    vmi.debug(t,"NAME")
     t[0] = t[1]
     
 
--- vm.py
+++ vm.py
@@ -1,43 +1,113 @@
-'''
-interface for emulating a vm with stack and functions
-'''
 class VirtualMachine:
+    '''
+    Interface for emulating a simple vm running on top of mindustry microprocessor
+    with stack and functions support
+    '''
     def __init__(self):
         self.stack = Stack(self)
+        self.vtable = VTable(self)
 
-        self.code = []   #stores temp code generated from vm translation layer
-        self.source = [] #stores whole source
+        self._debug = False
+
+        self.source = [] #stores whole source once has been all computed
 
         self.sysfunc = ['print','printflush']
-    
+        
     def print(self):
         self.stack.print()
     
+    def openBlock(self):
+        self.stack.pushJump()
+    
+    def closeBlock(self):
+        return self.stack.popJump()
+    
     def encode(self, code:str):
-        self.code.append(code)
+        if self.stack.getContext() == None:
+            print("ERROR: Empty stack found!!!")
+        else:
+            self.stack.encodeOnLocalBlock(code)
     
     def funcall(self,fname,fparams=None):
-        self.stack.funcall(fname,fparams)
+        self.vtable.funcall(fname,fparams)
+    
+    def flushBlock(self,block):
+        self.source.extend(block.getSource())
 
-    #flush temp code to source
+    #flush stack generated code to source
     def flush(self):
-        self.source.extend(self.code)
+        block = self.stack.pop()
+        self.source.extend(block.getCode(0))
 
     def getSource(self):
         self.flush()
         return '\n'.join(self.source)
+    
+    def debug(self,t,s):
+        out = ""
+        if(self._debug):
+            out += s
+            if(len(t)>1):
+                out+="\t\t\t" + str(t[1])
+            print(out)
 
-'''
-handles functions tables
-'''
+class Block:
+    '''
+    Structure representing a code block
+    '''
+    def __init__(self,vmi):
+        self.vmi = vmi
+        self.code = []
+        self.varTranslation = []
+    
+    def getSize(self):
+        return len(self.code)
+
+    def getCode(self, offset):
+        return self.code
+    
+    def translateVariable(self, vfrom, vto):
+        if [vfrom, vto] in self.varTranslation:
+            return
+        self.varTranslation.append([vfrom,vto])
+    
+    def encode(self, code):
+        self.code.append(code)
+    
+    def __str__(self):
+        out = "---CODE BLOCK---\n"
+        for i in range(len(self.code)):
+            out += str(i) + "| " + self.code[i] + "\n"
+        return out
+    
+    
 class VTable:
-    def __init__(self):
+    '''
+    Manages a function virtual table, bindings function name
+    to definition address.
+    Handles also builtin system functions.
+    '''
+    def __init__(self, vmi):
+        self.vmi = vmi
         self.functions = []
 
     def addFunctionToVTable(self, name:str, params:[]):
         self.functions.append([name, params, -1])
+    
+    def funcall(self,fname,params=None):
+        params = params.split()
+        if fname == "print":
+            self.vmi.encode("print {}".format(params[0]))
+        elif fname == "flush":
+            self.vmi.encode("printflush {}".format(params[0]))
+        elif fname == "test":
+            self.vmi.encode("test {}".format(" ".join(params)))
 
 class Stack:
+    '''
+    Simulates a complete stack with variable translation
+    for function calling and recursion.
+    '''
     def __init__(self, vmi):
         self.vmi = vmi
         self.stack = []
@@ -45,7 +115,14 @@ class Stack:
 
         self.push()
         
-        self.tempList = []
+        self.tempList = [] # used to provide temp variables to vm
+        self.jumpList = [] # used to execute if / while / for ...
+
+    def pushJump(self):
+        self.jumpList.append(self.getContext().getSize()) #append line number
+    
+    def popJump(self):
+        return self.jumpList.pop()
 
     def requestTempVar(self):
         i = 0
@@ -61,30 +138,30 @@ class Stack:
 
         self.tempList.remove(varname)
 
-    def funcall(self,fname,params=None):
-        params = params.split()
-        if fname == "print":
-            self.vmi.encode("print {}".format(params[0]))
-        elif fname == "flush":
-            self.vmi.encode("printflush {}".format(params[0]))
-        elif fname == "test":
-            self.vmi.encode("test {}".format(" ".join(params)))
-
     def getContext(self):
+        if self.currentStack <0:
+            return None
         return self.stack[self.currentStack]
+    
+    def getDepth(self):
+        return self.currentStack+1
 
     def addVariable(self, name:str, value):
-        self.getContext().append([name,value])
+        pass #FIXME
+
+    def encodeOnLocalBlock(self, code):
+        self.getContext().encode(code)
 
     #push new variable stack block
     def push(self):
-        self.stack.append([])
+        b = Block(self.vmi)
+        self.stack.append(b)
         self.currentStack += 1
     
     #remove upper variable block
     def pop(self):
-        self.stack.pop()
-        self.currentStack -= 0
+        self.currentStack -= 1
+        return self.stack.pop()
     
     def print(self):
         for i in self.stack:
